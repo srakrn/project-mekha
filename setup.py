c6ed2eee1610f3d6f6d9c8aa16b7ad74fa2d1dad
Sirakorn Lamyai <sirakorn.lamyai@adastragrp.com>
2022-05-12 14:04:45 +0700

    Initialising Python setup and project files

---
+++ setup.py
@@ -0,0 +1,11 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup(
+    setup_requires=['setup.cfg'],
+    setup_cfg=True
+)
